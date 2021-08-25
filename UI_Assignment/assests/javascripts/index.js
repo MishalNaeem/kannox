894d0d434f514c699a0c23558fb42c0727e569c9
MishalNaeem <89064921+MishalNaeem@users.noreply.github.com>
2021-08-25 16:39:11 +0500

    Update index.js

--- UI_Assignment/assests/javascripts/index.js
+++ UI_Assignment/assests/javascripts/index.js
@@ -6,10 +6,10 @@ hamburger.addEventListener("click",mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
